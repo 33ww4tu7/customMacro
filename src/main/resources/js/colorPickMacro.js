b62837cbdcf2d679631e7708e35d30788535f50c
Fando <p.fando@itransition.com>
2019-03-15 13:39:07 +0300

    suicide

--- src/main/resources/js/colorPickMacro.js
+++ src/main/resources/js/colorPickMacro.js
@@ -1,30 +1,20 @@
 AJS.$(function () {
     AJS.$('.ffi input[type="file"]').fancyFileInput();
+    AJS.$('#comment-save-button').click(function () {
+        updateImage(" ");
+        return false;
+    });
 });
 
-function updateImage(input, pageId) {
-    var file = input.files[0];
-    console.log(file);
-    var formdata = new FormData();
-    formdata.append("file", file);
-    formdata.append("comment", "background-page");
-    formdata.append("minorEdit", "true");
+function updateImage(input) {
     AJS.$.ajax({
-        url: "http://localhost:1990/confluence/rest/api/content/" + pageId + "/child/attachment",
+        url: 'http://localhost:1990/confluence',
         type: "POST",
-        contentType: false,
-        data: formdata,
-        processData: false,
-        headers: {
-            "Authorization": "Basic " + btoa("admin:admin"),
-            "X-Atlassian-Token": "nocheck"
-        },
-
         success: [function () {
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = function () {
-                    $('#main').css({"background-image": "url(" + this.result + ")"})
+                    $('#main').css({"background-image": "url("+ this.result +")"})
                 };
                 reader.readAsDataURL(input.files[0]);
                 alert(input.files[0].toString());
@@ -34,7 +24,4 @@ function updateImage(input, pageId) {
             alert("fuck")
         }]
     })
-    //}
 }
-
-
