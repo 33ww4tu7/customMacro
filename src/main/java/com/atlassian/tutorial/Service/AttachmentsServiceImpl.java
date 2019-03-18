8189803b0b122266a1bf9382280f0e0f70966e37
Fando <p.fando@itransition.com>
2019-03-18 16:14:48 +0300

    imporoved AJAX logic

---
+++ src/main/java/com/atlassian/tutorial/Service/AttachmentsServiceImpl.java
@@ -0,0 +1,52 @@
+package com.atlassian.tutorial.Service;
+
+
+import com.atlassian.activeobjects.external.ActiveObjects;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.tutorial.entity.AttachmentsEntity;
+import net.java.ao.Query;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Lists.newArrayList;
+
+@Scanned
+@Named
+public class AttachmentsServiceImpl implements AttachmentsService {
+    @ComponentImport
+    private final ActiveObjects ao;
+
+    @Inject
+    public AttachmentsServiceImpl(ActiveObjects ao) {
+        this.ao = checkNotNull(ao);
+    }
+
+    @Override
+    public AttachmentsEntity add(String path, String pageId, String userId) {
+        final AttachmentsEntity attachmentsEntity = ao.create(AttachmentsEntity.class);
+        attachmentsEntity.setPath(path);
+        attachmentsEntity.setPageId(pageId);
+        attachmentsEntity.setUserId(userId);
+        attachmentsEntity.save();
+        return attachmentsEntity;
+    }
+
+    @Override
+    public ArrayList<String> getUrl(String pageId, String userId) {
+        ArrayList<String> pathList = newArrayList();
+        AttachmentsEntity[] attachmentsEntity = ao.find(AttachmentsEntity.class, Query.select().where("PAGE_ID = ? AND USER_ID = ?", pageId, userId));
+        for (AttachmentsEntity ae : attachmentsEntity) {
+            pathList.add(ae.getPath());
+        }
+        return pathList;
+    }
+
+    public List<AttachmentsEntity> getAll() {
+        return newArrayList(ao.find(AttachmentsEntity.class));
+    }
+}
