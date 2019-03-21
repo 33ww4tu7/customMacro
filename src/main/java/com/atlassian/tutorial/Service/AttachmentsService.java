6d959096e204410a725afa44cf14ee5b2c6b9f41
Fando <p.fando@itransition.com>
2019-03-21 20:50:27 +0300

    release version

---
+++ src/main/java/com/atlassian/tutorial/Service/AttachmentsService.java
@@ -0,0 +1,17 @@
+package com.atlassian.tutorial.Service;
+
+import com.atlassian.activeobjects.tx.Transactional;
+import com.atlassian.tutorial.entity.AttachmentsEntity;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Transactional
+public interface AttachmentsService
+{
+    AttachmentsEntity add(String path, String pageId, String userId) throws SQLException;
+
+    String getUrl(String pageId, String userId) throws SQLException;
+
+    List<AttachmentsEntity> getAll();
+}
